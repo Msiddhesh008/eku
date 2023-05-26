9bbcc52197fcd09abf0ab1cf4f2929dfee8f7c9f
Siddhesh More <113719150+Msiddhesh008@users.noreply.github.com>
2023-05-26 12:15:18 +0530

    Add files via upload

---
+++ Cards.js
@@ -0,0 +1,37 @@
+import React from "react";
+import CardItems from "./CardItems";
+import { DetailFeatures } from "./Content";
+import './style.css'
+
+const Cards = () => {
+  return (
+    <>
+    <div className="container-fluid cards my-3 ">
+      <h2 style={{fontWeight:'bolder', color:' #257e9e'}}>Destinations</h2>
+      <div className="row">
+        {DetailFeatures.map((features)=>{
+          return  <div className="col-md-4">
+            <CardItems tittle={features.tittle} desc={features.desc} imgUrl={features.img}/>
+            </div>
+        })}
+      </div>
+    </div>
+
+        <div className="container-fluid">
+          <div className="container">
+            <div className="row">
+              
+        {DetailFeatures.map((features)=>{
+          return  <div className="col-md-4">
+            <CardItems tittle={features.tittle} desc={features.desc} imgUrl={features.img}/>
+            </div>
+        })}
+            </div>
+          </div>
+
+        </div>
+        </>
+  );
+};
+
+export default Cards;
